07e6d0e51a1f50fc0f568f8c851decd6bb25f860
AzarHai <154465808+AzarHai@users.noreply.github.com>
2024-07-06 23:40:03 +0300

    парочка изменений

--- practice/index.php
+++ practice/index.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>GordonRamsayLoves</title>
-    <link rel="shortcut icon" href="path/to/icon.png" />
-    <link rel="stylesheet" href="./header.css" />
+    <title>"Дора" — Проектирование высокоэффективных систем насосов</title>
+    <link rel="icon" href="water_pump_icon_229881.ico">
+    <link rel="stylesheet" href="./header.css" !important;/>
     <style>
+      .brand-icon {
+    width: 125px; /* Задайте нужный размер значка */
+    height: 125px; /* Задайте нужный размер значка */
+    margin-right: 32px; /* Отступ справа от значка */
+    margin-left: 32px; /* Отступ справа от значка */
+  }
       body {
         padding-top: 85px; /* Отступ сверху, равный высоте header */
       }
@@ -21,6 +27,94 @@
       .preserve-whitespace {
         white-space: pre-wrap; /* Сохраняем пробелы и переносы строк */
       }
+      .carousel-item {
+        height: 32rem;
+        background:#000;
+        color:white;
+        position: relative;
+        background-position:center;
+        background-size:cover;
+      }
+      .container{
+        position:absolute;
+        bottom:0;
+        left:0;
+        right:0;
+        padding-bottom: 50px;
+      }
+      .overlay-image{
+        position:absolute;
+        bottom:0;
+        left:0;
+        right:0;
+        top:0;
+        background-position:center;
+        background-size:cover;
+        opacity:0.5;
+        background-repeat: no-repeat;
+      }
+      .link-container {
+        width: 224px;
+        height: 114px; /* Автоматическая высота */
+        background-color: #898c8a; /* Серый цвет фона для области ссылок */
+        display: block;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        margin-bottom: 10px;
+        margin-right: 20px; /* Уменьшенный отступ справа */
+        margin-left: 200px; /* Сдвиг влево */
+        margin-top: -220px; /* Поднятие вверх */
+      }
+
+      .featurette-text {
+          margin-bottom: 10px; /* Отступ снизу между блоками */
+      }
+
+      .featurette-text a {
+          display: block; /* Делаем ссылки блочными элементами */
+          padding: 5px; /* Внутренний отступ вокруг текста ссылки */
+          color: black; /* Цвет ссылок */
+          line-height: 0.55; /* Высота строки */
+      }
+
+      .row.featurette {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap; /* Разрешаем перенос элементов */
+        margin-left: -250px; /* Сдвиг влево */
+      }
+      .col-md-7 {
+        flex: 1;
+        min-width: 200px; /* Минимальная ширина для адаптивности */
+      }
+      .col-md-5 {
+        flex: 0.5;
+        min-width: 200px; /* Минимальная ширина для адаптивности */
+      }
+      .brand-header .tagline {
+        font-size: 0.8em; 
+        margin-top: 70px; 
+        margin-left: -690px; 
+        color: black; 
+        position: relative;
+        
+      }
+      .brand-header .contact-info1 {
+        position: absolute; /* Абсолютное позиционирование */
+        left: 950px; /* Сдвиг влево */
+        top: 30%; /* Вертикальное выравнивание */
+        transform: translateY(-50%); /* Центрирование по вертикали */
+        font-size: 0.6em; 
+      }
+      .brand-header .contact-info2 {
+        position: absolute; /* Абсолютное позиционирование */
+        left: 1000px; /* Сдвиг влево */
+        top: 15%; /* Вертикальное выравнивание */
+        transform: translateY(-50%); /* Центрирование по вертикали */
+        font-size: 0.6em; 
+      }
     </style>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
@@ -30,16 +124,19 @@
     />
     <link rel="stylesheet" href="./style.css" />
     <script src="https://api-maps.yandex.ru/2.1/?apikey=98b95686-dd86-4029-b8f1-40b354e9c474&lang=ru_RU" type="text/javascript"></script>
-  </head>
-  <body>
+</head>
+  <body style="background-color:#0f5e9c;">
     <header>
-      <div class="brand-header">
-        <a class="navbar-brand" href="/index.php">
-          <img src="4070419.png" alt="Icon" class="brand-icon">
-          GordonRamsayLoves
-        </a>
-      </div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="brand-header">
+            <a class="navbar-brand" href="/index.php">
+              <img src="4070419.png" alt="Icon" class="brand-icon">
+            </a>
+            <p1 class="company-name"style="font-size: 1.5em;color: #0f3e8c;" >ЗАО Производственная фирма «Дора» </p1>
+            <p2 class="tagline">Новейшие технологии</p2>
+            <p3 class="contact-info1">Телефон: +7 (XXX) XXX-XX-XX</p3>
+            <p4 class="contact-info2">Email: info@dora.com</p4>
+        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-xxl">
           <button
             class="navbar-toggler"
@@ -83,69 +180,91 @@
               <button class="search-button" type="button" id="search-button">
                 <img src="лупа.png" alt="Icon" class="brand-search">
               </button>
+              <form action="search.php" method="post">
               <input type="text" class="form-control" id="search-input" placeholder="Поиск...">
+      </form>
             </div>
           </div>
         </div>
       </nav>
     </header>
-
     <section>
-      <div class="container">
-        <h2>Секция по центру страницы</h2>
-        <p class="preserve-whitespace"> Текст вашей секции...
-автоа
-авто
+      <div id="myCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+          <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+          <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-inteval="2000">
+            <div class="overlay-image"style="background-image:url(./slider-1.jpg);"></div>
+            <div class="container">
+              <h1>Example</h1>
+              <p>Kodfkfo DLFfsdf,sldf, sdfkldsf flsdpdl</p>
+              <a href="" class="btn btn-lg btn-primary">Sign up today</a>
+            </div>
+          </div>
+          <div class="carousel-item"data-inteval="1000">
+            <div class="overlay-image"style="background-image:url(./slide-2.png);width:1000px;height=400px;"></div>
+            <div class="container">
+              <h1>Example</h1>
+              <p>Kodfkfo DLFfsdf,sldf, sdfkldsf flsdpdl</p>
+              <a href="" class="btn btn-lg btn-primary">Sign up today</a>
+            </div>
+          </div>
+          <div class="carousel-item"data-inteval="500">
+            <div class="overlay-image"style="background-image:url(./slider-3.jpg);width:1200px;height=400px;"></div>
+            <div class="container">
+              <h1>Example</h1>
+              <p>Kodfkfo DLFfsdf,sldf, sdfkldsf flsdpdl</p>
+              <a href="" class="btn btn-lg btn-primary">Sign up today</a>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </a>
+      </div>
+      <hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">Гарантии качества </h2>
+          <p class="lead">
+            Наша компания работает с 1996 года. Основными клиентами являются приборостроительные заводы, заводы точного машиностроения,предприятия авиационно-космической техники, центры стандартизации и метрологии, научно-исследовательские институты и учреждения высшего образования.
 
-автоа
-а
+            Мы поставляем и внедряем измерительный инструмент и системы лучших мировых производителей.
 
-автоа
-автоа
-автоа
-автоа
-а
-а
+            Наши инженеры по оборудованию решают сложнейшие измерительные задачи и обучают заказчиков.
 
-а
+            Наша сервисная служба обеспечивает постоянную техническую поддержку всего поставляемого оборудования.
 
+            Наш коммерческий отдел всегда заботится о наших заказчиках.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-f
-
-        </p>
+            Все это позволяет нам быть одним из крупнейших поставщиков измерительной техники в РФ и странах СНГ.</p>
+        </div>
+        <div class="col-md-5 order-md-1 d-flex justify-content-center align-items-center">
+          <div class="link-container">
+            <div class="featurette-text">
+              <a href="https://www.youtube.com/">Мы на Ютуб</a>
+            </div>
+            <div class="featurette-text">
+              <a href="https://www.example.com/journal">Журнал НИТ</a>
+            </div>
+            <div class="featurette-text">
+              <a href="https://www.example.com/catalogs">Каталоги и брошюры</a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
-
     <footer>
       <p>&copy 2023-2024 by Arturchik and Ivan</p>
     </footer>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script>
       document.addEventListener('DOMContentLoaded', function() {
@@ -169,9 +288,6 @@ f
           }
         });
       });
-     
- 
-
     </script>
   </body>
 </html>
